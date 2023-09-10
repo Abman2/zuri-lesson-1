a840d64a6ceba5143bd07da9ef5585f015c1f95d
Abdulrahman <113942757+Abman2@users.noreply.github.com>
2023-09-10 20:56:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-function get(){return new Date().getTime()}
- 
-setInterval(() => { const time = get();
-    document.getElementById("currentUTCTime").textContent= time;
-},1)
+function getCurrentTimeInMillis() {
+  return new Date().getTime();
+}
+
+function updateCurrentTime() {
+  const currentTime = getCurrentTimeInMillis();
+  const currentTimeMillisElement = document.getElementById("currentUTCTime");
+  currentTimeMillisElement.textContent = currentTime;
+}
+
+// Call the updateCurrentTime function initially to set the content
+updateCurrentTime();
+
+// Refresh the content every 1 second (1000 milliseconds)
+setInterval(updateCurrentTime, 1000);
+
 const d = new Date().getDay()
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 document.getElementById("currentDayOfTheWeek").textContent = days[d]

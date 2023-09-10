e54cf52feb47c70aaf8f294bec89e919a3e396ed
Abdulrahman <113942757+Abman2@users.noreply.github.com>
2023-09-10 18:34:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+const time =new Date().getTime()
+document.getElementById("currentUTCTime").textContent = time
+setTimeout(() => {
+    window.location.reload();
+},1000)
+const d = new Date().getDay()
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+document.getElementById("currentDayOfTheWeek").textContent = days[d]
+
+})

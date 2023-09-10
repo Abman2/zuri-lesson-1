db83967a77e3d4ddfa8776c1f3a0b91d8664227b
Abdulrahman <113942757+Abman2@users.noreply.github.com>
2023-09-10 18:50:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const time =new Date().getTime()
 document.getElementById("currentUTCTime").textContent = time
 setTimeout(() => {
     window.location.reload();
-},1000)
+},2000)
 const d = new Date().getDay()
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 document.getElementById("currentDayOfTheWeek").textContent = days[d]

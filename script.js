6ae172983484d7894a1de35b33020095f346636d
Abdulrahman <113942757+Abman2@users.noreply.github.com>
2023-09-10 20:23:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-const time =new Date().getTime()
-document.getElementById("currentUTCTime").textContent = time
-setTimeout(() => {
-    window.location.reload();
-},2000)
+function get{let time =new Date().getTime()}
+document.getElementById("currentUTCTime").textContent = time;
+let t=setTimeout(() => {
+    get();
+},1)
 const d = new Date().getDay()
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 document.getElementById("currentDayOfTheWeek").textContent = days[d]
